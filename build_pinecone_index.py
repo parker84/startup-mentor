686efc0bdc44b9fc47233fe356c6fb0d85447a36
parker84 <brydonparker4@gmail.com>
2023-06-10 09:36:24 -0400

    v1 building of pinecone index based on pdf books (its running)

--- build_pinecone_index.py
+++ build_pinecone_index.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 from decouple import config
 from langchain.embeddings.openai import OpenAIEmbeddings
 from constants import EMBEDDING_MODEL
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
 from dotenv import find_dotenv, load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
@@ -41,29 +41,24 @@ index = pinecone.Index(config('PINECONE_INDEX_NAME'))
 def create_index(index, folder_path):
     logger.info(f'index stats before we start: \b{index.describe_index_stats()}')
 
-    txt_files = [file for file in os.listdir(folder_path) if file.endswith(".txt")]
-    for filename in tqdm(txt_files):
+    pdf_files = [file for file in os.listdir(folder_path) if file.endswith(".pdf")]
+    for filename in tqdm(pdf_files):
         logger.info(f'Loading and Splitting Book: {filename}')
         file_path = os.path.join(folder_path, filename)
-        loader = TextLoader(file_path=file_path, autodetect_encoding=True)
-        book = loader.load()
-
-        logger.debug('Splitting Book into Docs...')
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-        docs = text_splitter.split_documents(book)
-        n_docs = len(docs)
-        metadata = {
-            'book': filename
-        }
-
+        loader = PyPDFLoader(file_path=file_path)
+        pages = loader.load_and_split() # this allows us to track the page number
+        n_pages = len(pages)
         logger.info('Running Batches to Embed and Send into Index:')
-        for i in tqdm(range((n_docs // BATCH_SIZE) + 1)):
-            batch_text = [doc.page_content for doc in docs[i*BATCH_SIZE: (i+1)*BATCH_SIZE]]
+        for i in tqdm(range((n_pages // BATCH_SIZE) + 1)):
+            batch = pages[i*BATCH_SIZE: (i+1)*BATCH_SIZE]
+            batch_text = [page.page_content for page in batch]
             metadatas = [{
-                "chunk": j, "text": text, **metadata
-            } for j, text in enumerate(batch_text)]
+                "page": page.metadata['page'], 
+                "text": page.page_content,
+                "book": filename.strip('.pdf')
+            } for page in batch]
 
-            ids = [str(uuid4()) for _ in range(len(batch_text))]
+            ids = [str(uuid4()) for _ in range(len(batch))]
 
             logger.debug('Embedding...')
             embeds = embed.embed_documents(batch_text)
@@ -75,4 +70,4 @@ def create_index(index, folder_path):
 
 
 if __name__ == "__main__":
-    create_index(index, './data/got-books')
+    create_index(index, './data/books-pdfs')
